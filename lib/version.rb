b98adda672fc1dcc994cdc79e0c4369435d1552f
Mark24 <mark.zhangyoung@gmail.com>
2025-11-30 20:50:28 +0800

    update: dep

--- lib/version.rb
+++ lib/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Sliceway
-  VERSION = File.read(File.expand_path('../../VERSION', __dir__)).strip
+  VERSION = File.read(File.expand_path('../VERSION', __dir__)).strip
 end
