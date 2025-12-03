cd1c37da5964f9abbe1cde2290627d200c7fde68
Mark24 <mark.zhangyoung@gmail.com>
2025-12-03 16:08:33 +0800

    update: version

--- frontend/src/components/VersionInfo.tsx
+++ frontend/src/components/VersionInfo.tsx
@@ -27,7 +27,7 @@ const VersionInfo: React.FC = () => {
     fetchVersion();
   }, []);
 
-  const frontendVersion = '1.0.0';
+  const frontendVersion = '1.2.0';
 
   return (
     <Space size="small" style={{ marginLeft: 'auto' }}>
